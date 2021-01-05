436ede89d8eb32adf86b89e2b8bd61a0ab6bef5a
AbdullahAlRasheedi <alkw444@gmail.com>
2021-01-05 20:49:09 +0300

    cw 7 completed

--- question1.js
+++ question1.js
@@ -1,1 +1,16 @@
 // قم بحل السؤال الأول هنا
+
+let pets = [`Cat` , `Bird` , `Casco` , `Hamsters` , `Doge`];
+pets.pop();
+pets.push("Rabbet");
+console.log(pets);
+pets[1] = "Doge";
+console.log(pets);
+
+let cat ={
+    name: `Chloe`,
+    age: `3 years old`,
+    type: `Burmese`,
+    emoji:`😺`,
+};
+console.log(cat.age)
